94d748970c83086af92ae19fd79a22184f7e529d
Little Panda <>
2022-03-06 20:37:29 -0700

    Add Wrong question Cycle in LearnDeck

--- src/components/Deck/Learn/LearnDeck.js
+++ src/components/Deck/Learn/LearnDeck.js
@@ -3,24 +3,40 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router";
 import Question from "../../question/Question";
 import Button from "@mui/material/Button";
+import { updatePriorityScore } from "../../../redux/actions/actions";
 
 function LearnDeck() {
+  const jwt = useSelector((state) => state.userInfo).jwt;
+  const dispatch = useDispatch();
   const { id } = useParams();
-  const deck = useSelector((state) => state.deck.find((deck) => deck.id == id));
+  const fetchedDeck = useSelector((state) => state.deck.find((deck) => deck.id == id));
+  const [deck, setDeck] = useState(fetchedDeck);
   const navigate = useNavigate();
   const [idx, setIdx] = useState(0);
   const [completeQuestions, setCompleteQuestions] = useState(new Set());
+  const [wrongQuestions, setWrongQuestions] = useState(new Set());
 
   const handleOnClick = async () => {
+    for (var questionId of wrongQuestions) {
+      console.log("iterate set ", questionId);
+      const thisQuestion = deck.questions.find((question) => question.id == questionId);
+      dispatch(updatePriorityScore(jwt, { ...thisQuestion, priorityScore: Math.max(0, thisQuestion.priorityScore + 1) }, id));
+    }
     navigate(`/decks/${id}`);
   };
 
-  const setIndex = (sign) => {
-    if (sign == "asc") {
-      setIdx(Math.min(idx + 1, deck.questions.length));
-    } else if (sign == "desc") {
-      setIdx((idx - 1 + deck.questions.length) % deck.questions.length);
-    }
+  const setIndex = () => {
+    setIdx(Math.min(idx + 1, deck.questions.length));
+  };
+
+  const handleWrongQuestion = (questionIdx) => {
+    const questionId = deck.questions[questionIdx].id;
+    setWrongQuestions((prev) => {
+      const newSet = new Set(prev);
+      newSet.add(questionId);
+      return newSet;
+    });
+    setDeck({ ...deck, questions: [...deck.questions, deck.questions[questionIdx]] });
   };
 
   const inCompletedSet = (questionIdx) => {
@@ -33,7 +49,7 @@ function LearnDeck() {
 
       {deck == null ? (
         <h2>Loading</h2>
-      ) : idx == deck.questions.length ? (
+      ) : idx >= deck.questions.length ? (
         <h2>You've finished all the cards</h2>
       ) : (
         <Question
@@ -43,10 +59,12 @@ function LearnDeck() {
           questionIdx={idx}
           inCompletedSet={inCompletedSet}
           setIndex={setIndex}
+          handleWrongQuestion={handleWrongQuestion}
+          wrongQuestions={wrongQuestions}
         />
       )}
 
-      {deck != null && idx == deck.questions.length ? (
+      {deck != null && idx >= deck.questions.length ? (
         <Button onClick={() => handleOnClick()} sx={{ mt: 1, mr: 1 }} variant="outlined">
           END TEST
         </Button>
